03d04f3e9296916e986870bf21fe72975a8d38ad
Debaleena <debaleenaghosh30@gmail.com>
2026-01-09 20:36:48 +0530

    Fixed gateway authentication filter

--- ApiGateway/src/main/java/com/example/gateway/security/SecurityConfiguration.java
+++ ApiGateway/src/main/java/com/example/gateway/security/SecurityConfiguration.java
@@ -2,24 +2,21 @@ package com.example.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
 @EnableWebFluxSecurity
-public class SecurityConfiguration
-{
+@Configuration
+public class SecurityConfiguration {
+
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) throws Exception
-    {
-        httpSecurity
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable);
-        return httpSecurity.build();
+                .authorizeExchange(ex -> ex
+                        .anyExchange().permitAll()
+                )
+                .build();
     }
 }
